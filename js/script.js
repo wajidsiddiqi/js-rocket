ee6f951710364f7d5a0f75e61f82ed2799ed6342
wajidsiddiqi <120100446+wajidsiddiqi@users.noreply.github.com>
2022-12-11 20:01:12 +0500

    wrote some code :)

---
+++ js/script.js
@@ -0,0 +1,35 @@
+		var sayMyname = function (name) {
+			alert ('My name is: '+name);
+		}
+
+		var car = {
+			make: 'VW',
+			type: 'polo',
+			color: 'green',
+			isTurnedOn: false,
+			numberOfWheels: 4,
+			seats: [
+				'seat 1',
+				'seat 2',
+				'seat 3',
+				'seat 4',
+			],
+			turnOn: function () {
+				this.isTurnedOn = true;
+			},
+			fly: function () {
+				alert('fly');
+			},
+			switchCar: function (isOn) {
+				console.log('turn car '+isOn)
+				if (isOn == true) {
+					this.isTurnedOn = true;
+				}
+				else {
+					this.isTurnedOn = false;
+				}
+			}
+
+		};
+
+		console.log('Helloo jeee Khushaamdeed');
